184ab9bf6f45c7cf78c6c55ebec099d971f71df4
Nathan Pham <nathanpham.me@gmail.com>
2021-11-30 20:21:49 -0800

    zip pdf and try to upload

--- classes/Webtoon.js
+++ classes/Webtoon.js
@@ -1,5 +1,6 @@
 import PDFDocument from "pdfkit"
 import fetch from "node-fetch"
+import AdmZip from "adm-zip"
 import jsdom from "jsdom"
 
 import * as fs from "fs"
@@ -8,33 +9,33 @@ const { JSDOM } = jsdom
 
 export default class Webtoon {
 
-    
-    chapters = []
-
-    constructor(props) {
-
-        this.main(props)
-
-    }
-
-    async main({ name }) {
+    constructor({ name }) {
 
         this.name = name
-        this.url = this.createUrl(name)
-        
-        this.chapters = await this.fetchWebtoon(this.url)
-
-        for(const chapter of this.chapters) {
-
-            const pdfFilename = await this.fetchChapter(chapter)
-
-        }
 
     }
+
+    // async main({ name }) {
+        
+    //     this.chapters = await this.fetchWebtoon(this.url)
+
+    //     for(const chapter of this.chapters) {
+
+    //         const pdfFilename = await this.fetchChapter(chapter)
+
+    //     }
+
+    // }
 
     createUrl(name) {
 
         return `https://toonily.com/webtoon/${this.formatName(name)}`
+
+    }
+
+    createId() {
+
+        return (Math.floor(Math.random() * 10000) + 10000).toString().substring(1)
 
     }
     
@@ -53,28 +54,82 @@ export default class Webtoon {
 
     }
 
-    async fetchChapter(url) {
+    async fetchChapter(chapterUrl) {
 
-        const chapterHtml = await fetch(url).then(res => res.text())
+        const chapterHtml = await fetch(chapterUrl).then(res => res.text())
         const chapterDocument = new JSDOM(chapterHtml).window.document
 
-        const images = [...chapterDocument.querySelectorAll("img.wp-manga-chapter-img")].map(image => image.src)
+        const images = [...chapterDocument.querySelectorAll("img.wp-manga-chapter-img")].map(image => image.dataset.src.trim())
+        return images
+
+    }
+
+    async downloadImage(mangaDir, imageUrl) {
+
+        const imageFilename = `${mangaDir}/${this.createId()}.temp.${imageUrl.split('.').pop()}`
+
+        return new Promise(async (resolve, reject) => {
+
+            await fetch(imageUrl, {
+                method: 'GET',
+                headers: {
+                    "Origin": "https://toonily.com",
+                    "Referer": "https://toonily.com/",
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+                }
+            }).then(res => {
+
+                const imageStream = fs.createWriteStream(imageFilename)
+
+                res.body.pipe(imageStream)
+                res.body.on("end", () => resolve(imageFilename))
+
+                imageStream.on("error", () => reject("[Webtoon.js] failed to download image"))
+
+            })
+
+        })
         
-        const filename = `/manga/${this.formatName(this.name)}/${url.split('-').pop()}.pdf`
+    }
+
+    async createPdf(chapterUrl, imageUrls) {
+
+        const mangaDir = `./manga/${this.formatName(this.name)}`
+        const filename = `${mangaDir}/${parseInt(chapterUrl.split('-').pop())}.pdf`
+
+        if(!fs.existsSync(mangaDir)) fs.mkdirSync(mangaDir)
+
         const pdfDocument = new PDFDocument()
 
         pdfDocument.pipe(fs.createWriteStream(filename))
 
-        for(const image of images) {
-
-            const imageBuffer = Buffer.from(await fetch(image.url).then(res => res.arrayBuffer()))
-            pdfDocument.image(imageBuffer)
+        for(const imageUrl of imageUrls) {
             
+            const imageFilename = await this.downloadImage(mangaDir, imageUrl)
+            const pdfImage = pdfDocument.openImage(imageFilename)
+
+            console.log("downloaded", imageUrl)
+
+            pdfDocument.addPage({ size: [ pdfImage.width, pdfImage.height]  })
+            pdfDocument.image(pdfImage, 0, 0)
+
+            fs.unlinkSync(imageFilename)
+
+            // console.log("removed", imageFilename)
+
         }
 
         pdfDocument.end()
 
         return filename
+
+    }
+
+    zipPdf(pdfFilename) {
+
+        const compressedFile = new AdmZip()
+        compressedFile.addLocalFile(pdfFilename, "", "pdf")
+        return compressedFile.toBuffer()
 
     }
 
--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-import ReMarkable from "remarkable-cloud-js"
+import { Remarkable } from "remarkable-typescript"
+import * as dotenv from "dotenv"
+import * as fs from "fs"
+
 import Webtoon from "./classes/Webtoon.js"
+
+dotenv.config()
 
 // const webtoon = new Webtoon({ name: "The Max Level Hero has Returned" })
 
-const tablet = new ReMarkable()
+const main = (async () => {
 
-tablet.register_device("", ReMarkable.device_desc.desktop.linux).then(console.log)
+    const client = new Remarkable({ deviceToken: `${process.env.DEVICE_TOKEN}` })
+    await client.refreshToken()
+
+    const webtoon = new Webtoon({ name: "The Max Level Hero has Returned" })
+    // const chapters = (await webtoon.fetchWebtoon(webtoon.createUrl(webtoon.name))).reverse()
+
+    // const chapterUrl = chapters[0]
+    // const images = await webtoon.fetchChapter(chapterUrl)
+
+    // const pdfFilename = await webtoon.createPdf(chapterUrl, images)
+    // console.log(pdfFilename)
+    const pdfFilename = "./manga/the-max-level-hero-has-returned/1.pdf"
+    const zipBuffer = webtoon.zipPdf(pdfFilename)
+
+    await client.uploadZip('My document name', zipBuffer);
+    // await client.uploadPDF("bruh.pdf", fs.readFileSync())
+    // console.log("successfully uploaded!")
+    // const pdfUploadedId = await client.uploadPDF('name of PDF document', ID: '181a124b-bbdf-4fdd-8310-64fa87bc9c7f', pdfFileBuffer, /*optional UUID of parent folder*/);
+
+
+})()
 
 // let device_token = await rm_api.register_device('< one time code >', RmCJS.device_desc.desktop.linux)
